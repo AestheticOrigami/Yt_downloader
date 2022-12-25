58f3644c9d6c67b9c9e1afd7d24916e7f70cd336
AestheticOrigami <lberretta4@gmail.com>
2022-12-25 04:54:44 +0100

    first prototype of the script

---
+++ yt_downloader.py
@@ -0,0 +1,77 @@
+# importing packages
+from pytube import YouTube
+import os
+import time 
+import  argparse,time
+
+# url input from file list.txt
+
+#start time
+start_time = time.time()
+
+prec=""
+def single_download(url,destination='.'):
+    yt=YouTube(url)
+    try:
+            video = yt.streams.filter(only_audio=True).first()
+    except:
+            print("\n"+"Error in downloading:: " + yt.title+"\n")
+            return
+                
+    out_file = video.download(output_path=destination)
+    base, ext = os.path.splitext(out_file)
+    new_file = base + '.mp3'
+    try:
+        os.rename(out_file, new_file)
+    except:
+        os.remove(out_file)
+        print("song already downloaded:: " + yt.title)
+    base,ext = os.path.splitext(out_file)
+
+    print(yt.title + " has been successfully downloaded.")
+
+
+
+
+def list_download(path,destination='.'):
+    
+    
+    with open(path) as f:
+        global prec
+        lines = f.readlines()
+        
+        for line in lines:
+            
+            #check if line is duplicate
+            if line==prec:
+                print("duplicate line")
+                
+            prec=line
+            if line.strip():
+                single_download(line,destination)
+
+            else:
+                print("blank line")
+
+
+def main():
+    
+    # Check for any flags first
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-l", "--list", help="for list of song download")
+    ap.add_argument("-s", "--single", help="for single song download.")
+    ap.add_argument("-d", "--destination", help="destination folder")
+    args = vars(ap.parse_args())
+
+    if args["list"]:
+        list_download(args["list"],args["destination"])
+    elif args["single"]:
+        single_download(args["single"],args["destination"])
+
+
+def test_main():
+    list_download('A:\github\Yt_downloader\link.txt','A:\github\Yt_downloader\downloaded')
+if __name__ == '__main__':
+
+    test_main()  # run the main program
+    
